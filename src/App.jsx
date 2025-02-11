ecbab8869c791566aef95539b5cf4bbe0de0b60d
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-02-11 15:37:10 +0100

    added general css, fonts and imgs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import PageWelcome from './pages/welcome-page/welcome'//
+//DESCOMENTAR PARA AÑADIR LA PAGINA PRINCIPAL
+
+function App() {
+
+  return (
+    <div>
+      <></>
+    </div>
+  )
+}
+
+export default App
