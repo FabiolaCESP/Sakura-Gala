7b2c2fb22651d7957d3773f038a12d923dcb164e
fabiola canio espinoza <fcanioespinoza@gmail.com>
2025-02-12 12:54:27 +0100

    creating header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,22 @@
 import styles from '../header/Header.module.css'
 import cardicon from '../../assets/icons/card-icon-white.svg'
-
+import home from '../../assets/icons/home.svg'
+import Link from 'react-router'
 
 
 function Header(){
     return (
         <header className={styles.headerStyle}>
            <div className={styles.cardicon}>
-                <img src={cardicon} alt="cardiconwhite" />
+                <img src={cardicon} alt="icono de una carta de tarot" />
                  <h3>SAKURA TAROT</h3>
            </div>
 
            <div className={styles.Inicio}>
-               <img src= {home}alt="home" />
+                <Link to= '/'>
+               <img src={home} alt="icono de una casa" /></Link>
                <h3>Inicio</h3>
            </div>
-
-
-
-
 
         </header>
     )
