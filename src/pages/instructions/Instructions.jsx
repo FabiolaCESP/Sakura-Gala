cb43246c1ba28310552c9ac95f7de40db41f7682
Paola Morales <dissenyaspasia@gmail.com>
2025-02-12 15:35:32 +0100

    add imports to header and instructions

--- src/pages/instructions/Instructions.jsx
+++ src/pages/instructions/Instructions.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import InstructionsBox from '../../components/instructionsBox/InstructionsBox';
 import styles from './Instructions.module.css'
 import ButtonBig from '../../components/buttons/bigButton2/bigButton';
+import Header from '../../components/header/Header';
+
 
 function Instructions() {
     return (
         <>
+        <Header/>
         <div className={styles.induction}>
             <InstructionsBox />
             <ButtonBig 
