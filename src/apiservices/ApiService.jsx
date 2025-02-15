2ccfcc22cf1b7bc650680479695f78b28c740e40
Paola Morales <dissenyaspasia@gmail.com>
2025-02-15 11:36:18 +0100

    api service jsx

---
+++ src/apiservices/ApiService.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
+
+export const fetchAllCards = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener las cartas:", error);
+    return [];
+  }
+};
