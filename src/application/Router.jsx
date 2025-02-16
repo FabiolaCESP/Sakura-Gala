11835763fb9794958dced57a0e01b60ed2909d6a
SiGu <miskybox@gmail.com>
2025-02-16 12:36:18 +0100

    changes: settings Router

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router"
 import App from "../App"
 import Instructions from "../pages/instructions/Instructions"
 import TarotReading from "../components/TarotReading/TarotReading"
-
-
-import Instructions from "../pages/instructions/instructions"
-// import FanOfCards from "../components/FanOfCards/FanOfCards"
+import Instructions from "../pages/instructions/Instructions"
 import Favorites from "../pages/favorites/Favorites"
 
 function Router() {
@@ -15,7 +12,6 @@ function Router() {
                 <Route path="/" element={<App />}/>
                 <Route path="/instructions" element={<Instructions />}/>
                 <Route path="/tarotReading" element={<TarotReading />}/>
-                {/* <Route path="/reading" element={<FanOfCards />}/> */}
                 <Route path="/favorites" element={<Favorites />}/>
             </Routes>
         </BrowserRouter>
@@ -24,16 +20,3 @@ function Router() {
 
 export default Router
 
-
-{/* {/* <Route path="/discover" element={<Discover/>}/> */}
-
-// LINK FOR COMPONENTS //
-
-//import { Link } from 'react-router';
-
-{/* <Link to="/join">
-<button className="Join-Now">Join Now</button>
-</Link> */}
-
-// AÑADIR CADA LINK PARA CADA PÁGINA QUE SE QUIERE LINKEAR
-//PARA CADA ANCHOR Y CADA BOTÓN QUE TENGA LINKS
