b9f86535483c5d249fd5b2b7f25d853890c3a878
SiGu <miskybox@gmail.com>
2025-02-18 10:01:23 +0100

    changes: settings css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         }
       >
         <img src={ReadingCardIcon} alt="Lectura" />
-        <span>  Lectura</span>
+        <span> Lecturas</span>
       </NavLink>
       <NavLink
         to="/favorites"
